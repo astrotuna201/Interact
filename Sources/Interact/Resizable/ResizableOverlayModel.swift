ede746b25f5bf0c5a0cf5c9bd0b9d613736861ff
hpaelike <hpaelike@marum.de>
2022-03-03 18:37:29 +0100

    ...

--- Sources/Interact/Resizable/ResizableOverlayModel.swift
+++ Sources/Interact/Resizable/ResizableOverlayModel.swift
@@ -86,8 +86,8 @@ public class ResizableOverlayModel<Handle: View>: ObservableObject {
                 let x = cos(self.angle)*value.translation.width  - sin(self.angle)*value.translation.height
                 let y = cos(self.angle)*value.translation.height + sin(self.angle)*value.translation.width
                 
-                //self.offset.width  += x
-                //self.offset.height += y
+                self.offset.width  += x
+                self.offset.height += y
                 
                 self.size.width  -= value.translation.width
                 self.size.height += value.translation.height
